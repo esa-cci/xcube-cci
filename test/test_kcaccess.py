b953c186e717a475365ffa742b011f20040247a1
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2025-01-14 12:42:07 +0100

    updated tests

--- test/test_kcaccess.py
+++ test/test_kcaccess.py
@@ -56,34 +56,18 @@ class CciKerchunkDataStoreTest(unittest.TestCase):
         self.assertIsNotNone(self.store.get_data_store_params_schema())
 
     def test_list_data_ids(self):
-        self.assertEqual(
-            [
-                'ESACCI-BIOMASS-L4-AGB-CHANGE-100m-2018-2017-fv4.0-kr1.1',
-                'ESACCI-BIOMASS-L4-AGB-CHANGE-100m-2019-2018-fv4.0-kr1.1',
-                'ESACCI-BIOMASS-L4-AGB-CHANGE-100m-2020-2010-fv4.0-kr1.1',
-                'ESACCI-BIOMASS-L4-AGB-CHANGE-100m-2020-2019-fv4.0-kr1.1',
-                'ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-2020-fv4.0-kr1.1',
-                'ESACCI-L3C_CLOUD-CLD_PRODUCTS-ATSR2_AATSR-199506-201204-fv3.0-kr1.1',
-                'ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_AM-199109-201612-fv3.0-kr1.1',
-                'ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_PM-198201-201612-fv3.0-kr1.1',
-                'ESACCI-L3C_SNOW-SWE-MERGED-19790102-20200524-fv2.0-kr1.1',
-                'ESACCI-L4_FIRE-BA-MODIS-20010101-20200120-fv5.1-kr1.2',
-                'ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-2015-v2.0.7b-kr1.1',
-                'ESACCI-LC-L4-PFT-Map-300m-P1Y-1992-2020-v2.0.8-kr1.1',
-                'ESACCI-LST-L3C-LST-MODISA-0.01deg_1MONTHLY_DAY-200207-201812-fv3.00-kr1.1',
-                'ESACCI-LST-L3C-LST-MODISA-0.01deg_1MONTHLY_NIGHT-200207-201812-fv3.00-kr1.1',
-                'ESACCI-LST-L3C-LST-MODIST-0.01deg_1MONTHLY_DAY-200003-201812-fv3.00-kr1.1',
-                'ESACCI-LST-L3C-LST-MODIST-0.01deg_1MONTHLY_NIGHT-200003-201812-fv3.00-kr1.1',
-                'ESACCI-LST-L3S-LST-IRCDR_-0.01deg_1MONTHLY_DAY-199508-202012-fv2.00-kr1.1',
-                'ESACCI-LST-L3S-LST-IRCDR_-0.01deg_1MONTHLY_NIGHT-199508-202012-fv2.00-kr1.1',
-                'ESACCI-PERMAFROST-L4-ALT-ERA5_MODISLST_BIASCORRECTED-AREA4_PP-1997-2002-fv03.0-kr1.1',
-                'ESACCI-PERMAFROST-L4-ALT-MODISLST_CRYOGRID-AREA4_PP-2003-2019-fv03.0-kr1.1',
-                'ESACCI-SOILMOISTURE-L3S-SSMS-ACTIVE-19910805-20211231-fv07.1-kr1.1',
-                'ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-19781101-20211231-fv07.1-kr1.1',
-                'ESACCI-SOILMOISTURE-L3S-SSMV-PASSIVE-19781101-20211231-fv07.1-kr1.1',
-                'ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED_ADJUSTED-19781101-20211231-fv07.1-kr1.1'
-            ],
-            self.store.list_data_ids()
+        data_ids = self.store.list_data_ids()
+        self.assertIn(
+            "ESACCI-BIOMASS-L4-AGB-CHANGE-100m-2018-2017-fv4.0-kr1.1",
+            data_ids
+        )
+        self.assertIn(
+            "ESACCI-L3C_CLOUD-CLD_PRODUCTS-MERIS-AATSR_ENVISAT-200301-201112_fv2.0_kr1.0",
+            data_ids
+        )
+        self.assertIn(
+            "CCI_GMB_GIS_DTU_fv1.5_kr1.0",
+            data_ids
         )
 
     def test_get_data_opener_ids(self):
@@ -126,15 +110,12 @@ class CciKerchunkDataStoreTest(unittest.TestCase):
 
     def test_search_data(self):
         search_results = list(self.store.search_data())
-        self.assertEqual(24, len(search_results))
+        self.assertEqual(164, len(search_results))
         for descriptor, data_id in zip(search_results, self.store.get_data_ids()):
             self.assertIsInstance(descriptor, DatasetDescriptor)
             self.assertEqual(data_id, descriptor.data_id)
             self.assertIsInstance(descriptor.data_type, DataType)
             self.assertIs(xr.Dataset, descriptor.data_type.dtype)
-            self.assertIsInstance(descriptor.bbox, tuple)
-            # self.assertIsInstance(descriptor.spatial_res, float)
             self.assertIsInstance(descriptor.dims, dict)
-            self.assertIsInstance(descriptor.coords, dict)
             self.assertIsInstance(descriptor.data_vars, dict)
             self.assertIsInstance(descriptor.attrs, dict)
